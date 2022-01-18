9d96ed571122e7da48ff8379f6cc1173e6d8f29d
Martin Widmann <martin.widmann@russmedia.com>
2022-01-18 17:38:16 +0100

    hotfix: broke linking

--- app/components/SingleMessage.tsx
+++ app/components/SingleMessage.tsx
@@ -18,7 +18,7 @@ export default function SingleMessage({ message }: { message: ThreadMessage }) {
 
   return (
     <div className={`border-b border-gray-700 darnk:border-gray-500 ${isCurrentClass}`} id={`msg${currentMessage}`}>
-      <Link to={`message / ${message.id} `} className={`block border-l-4 text-gray-900 dark:text-gray-100 visited:text-gray-500 dark:visited:text-gray-400 pr-2 py-2 ${isCurrentUserClass}`}>
+      <Link to={`message/${message.id}`} className={`block border-l-4 text-gray-900 dark:text-gray-100 visited:text-gray-500 dark:visited:text-gray-400 pr-2 py-2 ${isCurrentUserClass}`}>
         <div style={indentation} className="flex items-start justify-between">
           <div className="overflow-hidden">
             <p className="text-sm md:text-base font-semibold overflow-hidden whitespace-nowrap text-ellipsis">{message.title}</p>
