19af8c14490fcf8ce53e493139aeede0502881b5
Martin Widmann <martin.widmann@russmedia.com>
2022-02-14 16:41:44 +0100

    fix: spoiler button not always working

--- app/routes/board/$boardId/thread/$threadId/message/$messageId.tsx
+++ app/routes/board/$boardId/thread/$threadId/message/$messageId.tsx
@@ -21,7 +21,6 @@ export default function MessageDetail() {
   const [canEdit, setCanEdit] = useState<boolean>(false)
 
   const threadId = parseInt(params.threadId ?? '0')
-  console.log(`threadId im message`, threadId)
 
   const findMessageInThread = (m: ThreadMessage): ThreadMessage | undefined => {
     if (m.id === parseInt(params.messageId ?? '0')) {
@@ -78,26 +77,27 @@ export default function MessageDetail() {
     }
 
     const spoilerList: HTMLButtonElement[] = []
-    Array.from(document.querySelectorAll(`.spoiler_btn`)).forEach((s) => {
-      const newSpoilerButton = document.createElement(`button`)
-      newSpoilerButton.classList.add(`spoiler-button`)
-      newSpoilerButton.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 pointer-events-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    Array.from(messageRef.current!.querySelectorAll(`.spoiler_btn`)).forEach(
+      (s) => {
+        const newSpoilerButton = document.createElement(`button`)
+        newSpoilerButton.classList.add(`spoiler-button`)
+        newSpoilerButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 pointer-events-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
         </svg>
-        <div class="pointer-events-none">Spoiler</div>
       `
-      s.parentNode?.replaceChild(newSpoilerButton, s)
-      newSpoilerButton.addEventListener(`click`, spoiler)
-      spoilerList.push(newSpoilerButton)
+        s.parentNode?.replaceChild(newSpoilerButton, s)
+        newSpoilerButton.addEventListener(`click`, spoiler)
+        spoilerList.push(newSpoilerButton)
 
-      return () => {
-        spoilerList.forEach((b) => {
-          b.removeEventListener(`click`, spoiler)
-        })
+        return () => {
+          spoilerList.forEach((b) => {
+            b.removeEventListener(`click`, spoiler)
+          })
+        }
       }
-    })
-  }, [])
+    )
+  }, [messageRef.current, params.messageId])
 
   const authorId = message.authorId
   return (
